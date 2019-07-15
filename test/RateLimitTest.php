6382ca950b9719a7b5f06aa3b82f170928f0e360
Leandro Silva <leandro.silva@zooxcloud.com>
2019-07-15 13:38:09 -0400

    add tests

--- test/RateLimitTest.php
+++ test/RateLimitTest.php
@@ -2,183 +2,176 @@
 
 namespace LosMiddlewareTest\RateLimit;
 
-use LosMiddleware\RateLimit\RateLimitMiddleware;
-use PHPUnit\Framework\TestCase;
-use Zend\Diactoros\Response;
-use Zend\Diactoros\ServerRequest;
-use Zend\ServiceManager\ServiceManager;
-use Zend\Session\Container;
-use LosMiddleware\RateLimit\Storage\ArrayStorage;
 use LosMiddleware\RateLimit\Exception\MissingRequirement;
+use LosMiddleware\RateLimit\RateLimitMiddleware;
+use LosMiddleware\RateLimit\RateLimitOptions;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\SimpleCache\CacheInterface;
+use Zend\Diactoros\Response\JsonResponse;
+use Zend\Diactoros\ServerRequest;
+use Zend\ProblemDetails\ProblemDetailsResponseFactory;
 
 class RateLimitTest extends TestCase
 {
-    protected $middleware;
+    /** @var array */
+    private $cache = [];
+    /** @var RateLimitMiddleware */
+    private $middleware;
 
-    protected function setUp()
+    protected function setUp() : void
     {
-        $container = new ServiceManager([]);
-        $container->setService('config', [
-            'los_rate_limit' => [
-                'max_requests' => 2,
-                'reset_time' => 10,
-                'ip_max_requests' => 2,
-                'ip_reset_time' => 10,
-                'api_header' => 'X-Api-Key',
-                'trust_forwarded' => true,
-                'prefer_forwarded' => false,
-                'forwarded_headers_allowed' => [
-                    'Client-Ip',
-                    'Forwarded',
-                    'Forwarded-For',
-                    'X-Cluster-Client-Ip',
-                    'X-Forwarded',
-                    'X-Forwarded-For',
-                ],
-                'forwarded_ip_index' => null,
+        $options = new RateLimitOptions([
+            'max_requests' => 2,
+            'reset_time' => 10,
+            'ip_max_requests' => 2,
+            'ip_reset_time' => 10,
+            'api_header' => 'X-Api-Key',
+            'trust_forwarded' => true,
+            'prefer_forwarded' => false,
+            'forwarded_headers_allowed' => [
+                'Client-Ip',
+                'Forwarded',
+                'Forwarded-For',
+                'X-Cluster-Client-Ip',
+                'X-Forwarded',
+                'X-Forwarded-For',
             ],
+            'forwarded_ip_index' => null,
         ]);
-        //$factory = new RateLimitFactory();
-        //$this->middleware = $factory($container);
-        $config = $container->get('config');
-        $rateConfig = array_key_exists('los_rate_limit', $config) ? $config['los_rate_limit'] : [];
-        $this->middleware = new RateLimitMiddleware(new ArrayStorage(), $rateConfig);
+
+        $problemResponse = $this->createMock(ProblemDetailsResponseFactory::class);
+        $problemResponse->method('createResponseFromThrowable')->willReturn(new JsonResponse([], 429));
+
+        $storage = $this->createMock(CacheInterface::class);
+        $storage->method('get')->will($this->returnCallback([$this, 'getCache']));
+        $storage->method('set')->will($this->returnCallback([$this, 'setCache']));
+
+        $this->middleware = new RateLimitMiddleware($storage, $problemResponse, $options);
     }
 
-    /**
-     * @covers LosMiddleware\RateLimit\RateLimit::__construct
-     * @covers LosMiddleware\RateLimit\RateLimit::__invoke
-     */
     public function testNeedIpOuApiKey()
     {
         $request = new ServerRequest();
-        $response = new Response();
 
-        $outFunction = function ($request, $response) {
-            return $response;
-        };
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler->method('handle')->willReturn(new JsonResponse([]));
 
-        $this->setExpectedException(MissingRequirement::class);
-        call_user_func($this->middleware, $request, $response, $outFunction);
+        $this->expectException(MissingRequirement::class);
+        $this->middleware->process($request, $handler);
     }
 
-    /**
-     * @covers LosMiddleware\RateLimit\RateLimit::__invoke
-     */
     public function testAddHeadersForApiKey()
     {
         $request = new ServerRequest();
         $request = $request->withHeader('X-Api-Key', '123');
-        $response = new Response();
+        $response = new JsonResponse([]);
 
-        $outFunction = function ($request, $response) {
-            return $response;
-        };
-
-        $result = call_user_func($this->middleware, $request, $response, $outFunction);
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler->method('handle')->willReturn(new JsonResponse([]));
+        $result = $this->middleware->process($request, $handler);
 
         $this->assertNotSame($response, $result);
-        $this->assertArrayHasKey(RateLimit::HEADER_REMAINING, $result->getHeaders());
-        $this->assertArrayHasKey(RateLimit::HEADER_LIMIT, $result->getHeaders());
-        $this->assertArrayHasKey(RateLimit::HEADER_RESET, $result->getHeaders());
+        $this->assertArrayHasKey(RateLimitMiddleware::HEADER_REMAINING, $result->getHeaders());
+        $this->assertArrayHasKey(RateLimitMiddleware::HEADER_LIMIT, $result->getHeaders());
+        $this->assertArrayHasKey(RateLimitMiddleware::HEADER_RESET, $result->getHeaders());
 
-        $this->assertEquals(2, $result->getHeader(RateLimit::HEADER_REMAINING)[0]);
-        $this->assertLessThanOrEqual(10, $result->getHeader(RateLimit::HEADER_RESET)[0]);
-        $this->assertEquals(2, $result->getHeader(RateLimit::HEADER_LIMIT)[0]);
+        $this->assertEquals(2, $result->getHeader(RateLimitMiddleware::HEADER_REMAINING)[0]);
+        $this->assertLessThanOrEqual(10, $result->getHeader(RateLimitMiddleware::HEADER_RESET)[0]);
+        $this->assertEquals(2, $result->getHeader(RateLimitMiddleware::HEADER_LIMIT)[0]);
     }
 
-    /**
-     * @covers LosMiddleware\RateLimit\RateLimit::__invoke
-     * @covers LosMiddleware\RateLimit\RateLimit::getClientIp
-     */
     public function testAddHeadersForIp()
     {
         $request = new ServerRequest(['REMOTE_ADDR' => '127.0.0.1']);
         $request = $request->withHeader('X-Forwarded-For', '192.168.1.1');
-        $response = new Response();
+        $response = new JsonResponse([]);
 
-        $outFunction = function ($request, $response) {
-            return $response;
-        };
-
-        $result = call_user_func($this->middleware, $request, $response, $outFunction);
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler->method('handle')->willReturn(new JsonResponse([]));
+        $result = $this->middleware->process($request, $handler);
 
         $this->assertNotSame($response, $result);
-        $this->assertArrayHasKey(RateLimit::HEADER_REMAINING, $result->getHeaders());
-        $this->assertArrayHasKey(RateLimit::HEADER_LIMIT, $result->getHeaders());
-        $this->assertArrayHasKey(RateLimit::HEADER_RESET, $result->getHeaders());
+        $this->assertArrayHasKey(RateLimitMiddleware::HEADER_REMAINING, $result->getHeaders());
+        $this->assertArrayHasKey(RateLimitMiddleware::HEADER_LIMIT, $result->getHeaders());
+        $this->assertArrayHasKey(RateLimitMiddleware::HEADER_RESET, $result->getHeaders());
 
-        $this->assertEquals(2, $result->getHeader(RateLimit::HEADER_REMAINING)[0]);
-        $this->assertLessThanOrEqual(10, $result->getHeader(RateLimit::HEADER_RESET)[0]);
-        $this->assertEquals(2, $result->getHeader(RateLimit::HEADER_LIMIT)[0]);
+        $this->assertEquals(2, $result->getHeader(RateLimitMiddleware::HEADER_REMAINING)[0]);
+        $this->assertLessThanOrEqual(10, $result->getHeader(RateLimitMiddleware::HEADER_RESET)[0]);
+        $this->assertEquals(2, $result->getHeader(RateLimitMiddleware::HEADER_LIMIT)[0]);
     }
 
-    /**
-     * @covers LosMiddleware\RateLimit\RateLimit::__invoke
-     */
     public function testDecreaseRemaining()
     {
         $request = new ServerRequest();
         $request = $request->withHeader('X-Api-Key', '123');
-        $response = new Response();
 
-        $outFunction = function ($request, $response) {
-            return $response;
-        };
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler->method('handle')->willReturn(new JsonResponse([]));
+        $this->middleware->process($request, $handler);
+        $result = $this->middleware->process($request, $handler);
 
-        $result = call_user_func($this->middleware, $request, $response, $outFunction);
-        $result = call_user_func($this->middleware, $request, $response, $outFunction);
-
-        $this->assertEquals(1, $result->getHeader(RateLimit::HEADER_REMAINING)[0]);
-        $this->assertLessThanOrEqual(10, $result->getHeader(RateLimit::HEADER_RESET)[0]);
-        $this->assertEquals(2, $result->getHeader(RateLimit::HEADER_LIMIT)[0]);
+        $this->assertEquals(1, $result->getHeader(RateLimitMiddleware::HEADER_REMAINING)[0]);
+        $this->assertLessThanOrEqual(10, $result->getHeader(RateLimitMiddleware::HEADER_RESET)[0]);
+        $this->assertEquals(2, $result->getHeader(RateLimitMiddleware::HEADER_LIMIT)[0]);
     }
 
-    /**
-     * @covers LosMiddleware\RateLimit\RateLimit::__invoke
-     */
     public function testGenerate429()
     {
         $request = new ServerRequest();
         $request = $request->withHeader('X-Api-Key', '123');
-        $response = new Response();
 
-        $outFunction = function ($request, $response) {
-            return $response;
-        };
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler->method('handle')->willReturn(new JsonResponse([]));
 
-        $result = call_user_func($this->middleware, $request, $response, $outFunction);
-        $result = call_user_func($this->middleware, $request, $response, $outFunction);
-        $result = call_user_func($this->middleware, $request, $response, $outFunction);
-        $result = call_user_func($this->middleware, $request, $response, $outFunction);
+        $this->middleware->process($request, $handler);
+        $this->middleware->process($request, $handler);
+        $this->middleware->process($request, $handler);
+        $result = $this->middleware->process($request, $handler);
 
-        $this->assertArrayNotHasKey(RateLimit::HEADER_REMAINING, $result->getHeaders());
-        $this->assertArrayNotHasKey(RateLimit::HEADER_LIMIT, $result->getHeaders());
-        $this->assertLessThanOrEqual(10, $result->getHeader(RateLimit::HEADER_RESET)[0]);
+        $this->assertArrayNotHasKey(RateLimitMiddleware::HEADER_REMAINING, $result->getHeaders());
+        $this->assertArrayHasKey(RateLimitMiddleware::HEADER_LIMIT, $result->getHeaders());
+        $this->assertLessThanOrEqual(10, $result->getHeader(RateLimitMiddleware::HEADER_RESET)[0]);
         $this->assertSame(429, $result->getStatusCode());
     }
 
-    /**
-     * @covers LosMiddleware\RateLimit\RateLimit::__invoke
-     */
     public function testReset()
     {
-        $container = new Container('LosRateLimit');
-        $container->offsetSet('remaining', 0);
-        $container->offsetSet('created', strtotime('-20 second'));
+//        $container = new Container('LosRateLimit');
+//        $container->offsetSet('remaining', 0);
+//        $container->offsetSet('created', strtotime('-20 second'));
 
         $request = new ServerRequest();
         $request = $request->withHeader('X-Api-Key', '123');
-        $response = new Response();
 
-        $outFunction = function ($request, $response) {
-            return $response;
-        };
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler->method('handle')->willReturn(new JsonResponse([]));
+        $this->middleware->process($request, $handler);
+        $this->middleware->process($request, $handler);
 
-        $result = call_user_func($this->middleware, $request, $response, $outFunction);
+        $this->cache['123']['created'] = strtotime('-20 second');
+        $result = $this->middleware->process($request, $handler);
 
-        $this->assertArrayHasKey(RateLimit::HEADER_REMAINING, $result->getHeaders());
-        $this->assertArrayHasKey(RateLimit::HEADER_LIMIT, $result->getHeaders());
-        $this->assertLessThanOrEqual(10, $result->getHeader(RateLimit::HEADER_RESET)[0]);
+        $this->assertArrayHasKey(RateLimitMiddleware::HEADER_REMAINING, $result->getHeaders());
+        $this->assertArrayHasKey(RateLimitMiddleware::HEADER_LIMIT, $result->getHeaders());
+        $this->assertLessThanOrEqual(10, $result->getHeader(RateLimitMiddleware::HEADER_RESET)[0]);
+        $this->assertSame(200, $result->getStatusCode());
+    }
+
+    /**
+     * @param null|mixed $default
+     * @return null|mixed
+     */
+    public function getCache(string $key, $default = null)
+    {
+        return $this->cache[$key] ?? $default;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setCache(string $key, $value) : void
+    {
+        $this->cache[$key] = $value;
     }
 }
