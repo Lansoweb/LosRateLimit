f260a6ef8e32f384cea24cecade7a55fee23e927
AndreasMaros <andreas.maros@gebruederheitz.de>
2021-12-07 14:28:53 +0100

    [refactor] Separate test setup and tests

--- test/RateLimitTest.php
+++ test/RateLimitTest.php
@@ -12,44 +12,8 @@ use Laminas\Diactoros\Response\JsonResponse;
 use Laminas\Diactoros\ServerRequest;
 use Mezzio\ProblemDetails\ProblemDetailsResponseFactory;
 
-class RateLimitTest extends TestCase
+class RateLimitTest extends TestSetup
 {
-    /** @var array */
-    private $cache = [];
-    /** @var RateLimitMiddleware */
-    private $middleware;
-
-    protected function setUp() : void
-    {
-        $options = new RateLimitOptions([
-            'max_requests' => 2,
-            'reset_time' => 10,
-            'ip_max_requests' => 2,
-            'ip_reset_time' => 10,
-            'api_header' => 'X-Api-Key',
-            'trust_forwarded' => true,
-            'prefer_forwarded' => false,
-            'forwarded_headers_allowed' => [
-                'Client-Ip',
-                'Forwarded',
-                'Forwarded-For',
-                'X-Cluster-Client-Ip',
-                'X-Forwarded',
-                'X-Forwarded-For',
-            ],
-            'forwarded_ip_index' => null,
-        ]);
-
-        $problemResponse = $this->createMock(ProblemDetailsResponseFactory::class);
-        $problemResponse->method('createResponseFromThrowable')->willReturn(new JsonResponse([], 429));
-
-        $storage = $this->createMock(CacheInterface::class);
-        $storage->method('get')->will($this->returnCallback([$this, 'getCache']));
-        $storage->method('set')->will($this->returnCallback([$this, 'setCache']));
-
-        $this->middleware = new RateLimitMiddleware($storage, $problemResponse, $options);
-    }
-
     public function testNeedIpOuApiKey()
     {
         $request = new ServerRequest();
@@ -156,22 +120,5 @@ class RateLimitTest extends TestCase
         $this->assertArrayHasKey(RateLimitMiddleware::HEADER_LIMIT, $result->getHeaders());
         $this->assertLessThanOrEqual(10, $result->getHeader(RateLimitMiddleware::HEADER_RESET)[0]);
         $this->assertSame(200, $result->getStatusCode());
-    }
-
-    /**
-     * @param null|mixed $default
-     * @return null|mixed
-     */
-    public function getCache(string $key, $default = null)
-    {
-        return $this->cache[$key] ?? $default;
-    }
-
-    /**
-     * @param mixed $value
-     */
-    public function setCache(string $key, $value) : void
-    {
-        $this->cache[$key] = $value;
     }
 }
